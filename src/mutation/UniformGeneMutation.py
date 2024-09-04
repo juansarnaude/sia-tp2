5f96c7995de875be9d8a7186d953f5130d4e2c0f
bcanevaro <bcanevaro@itba.edu.ar>
2024-09-04 18:32:09 -0300

    Added uniform gene mutation.

---
+++ src/mutation/UniformGeneMutation.py
@@ -0,0 +1,18 @@
+from src.mutation.Mutation import Mutation
+from src.genes.Gene import Gene
+import random
+
+class SingleGene(Mutation):
+
+    @classmethod
+    def mutate(cls, genes:Gene, probability: float) -> Gene | None:
+        base_genes = genes.get_stat_array()
+        height_index = 0
+        for i in range(len(base_genes) - 1):
+            rand = random.uniform(0,1)
+            if rand < probability:
+                if i == height_index:
+                    base_genes[i] = random.uniform(1.3, 2.0)
+                else:
+                    base_genes[i] = random.uniform(0, 150)
+        return Gene(base_genes)
