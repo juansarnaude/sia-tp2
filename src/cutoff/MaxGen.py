7aeda0a16d8e8ca6b2b9112ac2de3277b0a1cd29
Gaston Alasia <galasia@itba.edu.ar>
2024-09-06 18:06:27 -0300

    feat: check for None arguments in cutoffs

--- src/cutoff/MaxGen.py
+++ src/cutoff/MaxGen.py
@@ -3,7 +3,20 @@ from src.classes.Individual import Individual
 from src.cutoff.Cutoff import Cutoff
 
 class MaxGen(Cutoff):
+    """
+    Structure cuts off if the new population has a relevant amount of individuals repeated within the past N generations.
+
+    Args:
+        old_population (List[List[Individual]]): Not needed.
+        new_population (List[Individual]): Not needed either.
+        generations (int): The number of the actual generation.
+        threshold (float): The number of maximum amount of generations at which the genetic algorithm should cut off.
+
+    Return:
+        bool: Genetic algorithm should cutoff in case of True.
+    """
 
     @classmethod
-    def cutoff(cls, old_population: List[Individual], new_population: List[Individual], generation: int, threshold: any) -> bool:
-        return generation >= threshold
+    def cutoff(cls, old_population:List[List[Individual]], new_population:List[Individual], generations: int, threshold: any) -> bool:
+        cls.validate_params(generations, threshold)
+        return generations >= threshold
