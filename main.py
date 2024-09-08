babead5deccf6d56181162f3b03f8572e5525d99
Gaston Alasia <galasia@itba.edu.ar>
2024-09-07 23:01:48 -0300

    feat: seed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import copy
 import math
+import random
 
 from utils.Config import Config
 from src.classes.PopulationInitializer import PopulationInitializer
@@ -9,48 +10,57 @@ def check_k(population_size: int, k: int):
     if k>population_size:
         raise ValueError("K selection must not be higher than population size")
 
-config = Config(sys.argv[1])
+if __name__ == "__main__":
 
-k = config.selection_k
-population_size = config.population_size
+    # Plant the seed 
+    random.seed('Que asco me da boca')
 
-check_k(population_size, k)
+    config = Config(sys.argv[1])
 
-generations = []
+    k = config.selection_k
+    population_size = config.population_size
 
-current_population = PopulationInitializer(config.population_size, config.character)
+    check_k(population_size, k)
 
-generations.append(copy.deepcopy(current_population))
+    generations = []
 
-while not config.cutoff(generations, current_population, len(generations), config.cutoff_threshold):
-    selection_count1 = math.ceil(k*config.selection_a)
-    selection_count2 = k-selection_count1
+    current_population = PopulationInitializer(config.population_size, config.character)
+    generations.append(copy.deepcopy(current_population))
 
-    current_population1 = config.selection1(current_population, selection_count1)
-    current_population2 = config.selection2(current_population, selection_count2)
+    num = 1
 
-    current_population = current_population1 + current_population2
+    while not config.cutoff(generations, current_population, len(generations), config.cutoff_threshold):
+        num = num +1
+        selection_count1 = math.ceil(k*config.selection_a)
+        selection_count2 = k-selection_count1
 
-    child_population = []
+        current_population1 = config.selection1(current_population, selection_count1)
+        current_population2 = config.selection2(current_population, selection_count2)
 
-    for i in range(len(current_population) - 1):
-        childs_genes = config.crossover(current_population[i].gene, current_population[i+1].gene)
-        child1_gene = childs_genes[0]
-        child2_gene = childs_genes[1]
-        child1_gene = config.mutation(child1_gene, config.mutation_probability)
-        child2_gene = config.mutation(child2_gene, config.mutation_probability)
-        child_population.append(config.character(child1_gene))
-        child_population.append(config.character(child2_gene))
+        current_population = current_population1 + current_population2
 
-    current_population = config.replacement(current_population, child_population, config.selection3, config.selection4, config.population_size, config.selection_b)
+        child_population = []
 
-    generations.append(current_population)
+        for i in range(len(current_population) - 1):
+            childs_genes = config.crossover(current_population[i].gene, current_population[i+1].gene)
+            child1_gene = childs_genes[0]
+            child2_gene = childs_genes[1]
+            child1_gene = config.mutation(child1_gene, config.mutation_probability)
+            child2_gene = config.mutation(child2_gene, config.mutation_probability)
+            child_population.append(config.character(child1_gene))
+            child_population.append(config.character(child2_gene))
 
-for i,generation in enumerate(generations):
-    print("Generation " + str(i) + ": ")
-    for individual in generation:
-        print(individual)
+        current_population = config.replacement(current_population, child_population, config.selection3, config.selection4, config.population_size, config.selection_b)
 
+        generations.append(current_population)
+
+    for i,generation in enumerate(generations):
+        print("Generation " + str(i) + ": ")
+        for individual in generation:
+            print(individual)
+
+    best_individual = max(current_population, key=lambda ind: ind.fitness)
+    print(f'generation n°: {num}, best_fitness: {best_individual.getPerformance()}')
     
 
     
