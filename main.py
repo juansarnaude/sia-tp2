51969f52b56458725842f9a171e103d4017cde40
Juan Segundo Arnaude <juansarnaude@gmail.com>
2024-09-08 11:28:40 -0300

    now prints in csv

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import csv
 import sys
 import copy
 import math
@@ -29,38 +30,44 @@ if __name__ == "__main__":
 
     num = 1
 
-    while not config.cutoff(generations, current_population, len(generations), config.cutoff_threshold):
-        num = num +1
-        selection_count1 = math.ceil(k*config.selection_a)
-        selection_count2 = k-selection_count1
 
-        current_population1 = config.selection1(current_population, selection_count1)
-        current_population2 = config.selection2(current_population, selection_count2)
 
-        current_population = current_population1 + current_population2
+    with open('output/output.csv', mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Generation N°','Fitness','Height','Strength','Dexterity','Intelligence', 'Vigor','Constitution'])
 
-        child_population = []
+        while not config.cutoff(generations, current_population, len(generations), config.cutoff_threshold):
+            num = num +1
+            selection_count1 = math.ceil(k*config.selection_a)
+            selection_count2 = k-selection_count1
 
-        for i in range(len(current_population) - 1):
-            childs_genes = config.crossover(current_population[i].gene, current_population[i+1].gene)
-            child1_gene = childs_genes[0]
-            child2_gene = childs_genes[1]
-            child1_gene = config.mutation(child1_gene, config.mutation_probability)
-            child2_gene = config.mutation(child2_gene, config.mutation_probability)
-            child_population.append(config.character(child1_gene))
-            child_population.append(config.character(child2_gene))
+            current_population1 = config.selection1(current_population, selection_count1)
+            current_population2 = config.selection2(current_population, selection_count2)
 
-        current_population = config.replacement(current_population, child_population, config.selection3, config.selection4, config.population_size, config.selection_b)
+            current_population = current_population1 + current_population2
 
-        generations.append(current_population)
+            child_population = []
 
-    for i,generation in enumerate(generations):
-        print("Generation " + str(i) + ": ")
-        for individual in generation:
-            print(individual)
+            for i in range(len(current_population) - 1):
+                childs_genes = config.crossover(current_population[i].gene, current_population[i+1].gene)
+                child1_gene = childs_genes[0]
+                child2_gene = childs_genes[1]
+                child1_gene = config.mutation(child1_gene, config.mutation_probability)
+                child2_gene = config.mutation(child2_gene, config.mutation_probability)
+                child_population.append(config.character(child1_gene))
+                child_population.append(config.character(child2_gene))
 
-    best_individual = max(current_population, key=lambda ind: ind.fitness)
-    print(f'generation n°: {num}, best_fitness: {best_individual.getPerformance()}')
+            current_population = config.replacement(current_population, child_population, config.selection3, config.selection4, config.population_size, config.selection_b)
+
+            generations.append(current_population)
+
+        for i,generation in enumerate(generations):
+            for individual in generation:
+                writer.writerow([i,individual.fitness,individual.gene.height,individual.gene.strength,individual.gene.dexterity,
+                                 individual.gene.intelligence,individual.gene.vigor,individual.gene.constitution])
+
+        best_individual = max(current_population, key=lambda ind: ind.fitness)
+        print(f'Generation n°: {num}, best_fitness: {best_individual.getPerformance()}')
     
 
     
